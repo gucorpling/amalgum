7d9fda94cd2fce5d113286e9e490f6aaf11d3413
Luke Gessler <lukegessler@gmail.com>
2019-11-04 15:16:50 -0500

    get 190 instead of 150 from each category: 150 gave 400k tokens

--- mediawiki_scraper/get_news.sh
+++ mediawiki_scraper/get_news.sh
@@ -1,1 +1,11 @@
-python main.py --config configs/wikinews.yaml --method scrape --output-dir ../out/news/ --stop-after 5 --cmtitle=Category:Asia --cmtitle=Category:North_America
+python main.py \
+  --config configs/wikinews.yaml \
+  --method scrape \
+  --output-dir ../out/news/ \
+  --stop-after 190 \
+  --cmtitle=Category:Africa \
+  --cmtitle=Category:Middle_East \
+  --cmtitle=Category:Asia \
+  --cmtitle=Category:Europe \
+  --cmtitle=Category:North_America \
+  --cmtitle=Category:South_America \
