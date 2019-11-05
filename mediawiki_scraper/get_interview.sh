74bf3916c6406763f0653a31a6dff2ad01609225
Luke Gessler <lukegessler@gmail.com>
2019-11-05 13:37:05 -0500

    missing line continuation

--- mediawiki_scraper/get_interview.sh
+++ mediawiki_scraper/get_interview.sh
@@ -3,6 +3,6 @@ python main.py \
   --config configs/wikinews_interview.yaml \
   --method scrape \
   --output-dir ../out/interview/ \
-  --stop-after 1000
+  --stop-after 1000 \
   --split-if-long
 
