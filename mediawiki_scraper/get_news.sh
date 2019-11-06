2f2e67e9ff2161278c2a4463a29d741001df6581
Luke Gessler <lukegessler@gmail.com>
2019-11-05 23:11:57 -0500

    update scripts

--- mediawiki_scraper/get_news.sh
+++ mediawiki_scraper/get_news.sh
@@ -1,8 +1,9 @@
+pkill -f parsoid
 python main.py \
   --config configs/wikinews.yaml \
   --method scrape \
   --output-dir ../out/news/ \
-  --stop-after 190 \
+  --stop-after 170 \
   --cmtitle=Category:Africa \
   --cmtitle=Category:Middle_East \
   --cmtitle=Category:Asia \
