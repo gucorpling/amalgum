f9106e46463472ed3c021690a527d8b7cc32b5ba
Luke Gessler <lukegessler@gmail.com>
2019-11-06 11:13:58 -0500

    ensure filename matches id

--- scripts/renumber.py
+++ scripts/renumber.py
@@ -2,8 +2,10 @@ import glob
 import sys
 import re
 import os
+from bs4 import BeautifulSoup
 
 NUMBER_PATTERN = re.compile(r"(\d+).xml")
+ID_PATTERN = re.compile(r' id="([^"]*)"')
 
 
 def pad(numstring, padding_length=3):
@@ -33,12 +35,30 @@ def main(d):
         for new_i, (old_i, filename) in enumerate(
             sorted(filenames_with_numbers, key=lambda x: x[0])
         ):
-            new_i = pad(str(new_i))
+            new_i = str(new_i)
             old_i = str(old_i)
             if new_i != old_i:
                 new_name = new_filename(filename, old_i, new_i)
                 print(f"Renaming:\t'{filename}' -> '{new_name}'")
                 os.rename(filename, new_name)
+
+                filename = new_name
+
+            with open(filename, "r") as f:
+                s = f.read()
+
+            match = re.search(ID_PATTERN, s)
+            if match is None:
+                print(f"{filename} doesn't have an id attribute!")
+                sys.exit(1)
+            elif match.group(1) != filename[:-4]:
+                print(
+                    f"<text>'s id attribute is {match.group(1)}, but filename is {filename[:-4]}"
+                )
+                s = s.replace(f' id="{match.group(1)}"', f' id="{filename[:-4]}"')
+                print("\tReplaced id attribute with the filename.")
+                with open(filename, "w") as f:
+                    f.write(s)
 
 
 if __name__ == "__main__":
