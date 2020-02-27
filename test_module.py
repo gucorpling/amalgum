20d667e6597939865ce667db4bd1e8c58e19c043
janetlauyeung <yl879@georgetown.edu>
2020-02-27 16:44:31 -0500

    Updated test_module.py
    test_stype, test_coref, test_ace, test_rst

--- test_module.py
+++ test_module.py
@@ -1,11 +1,12 @@
-# from nlp_modules.s_typer import test_main as test_stype
-# from nlp_modules.xrenner_coreferencer import test_main as test_coref
+from nlp_modules.s_typer import test_main as test_stype
+from nlp_modules.xrenner_coreferencer import test_main as test_coref
 from nlp_modules.ace_entities import test_main as test_ace
+from nlp_modules.rst_parser import test_main as test_rst
 
 from argparse import ArgumentParser
 
 p = ArgumentParser()
-p.add_argument("-m","--module",choices=["stype","coref", "ace"],default="ace",help="module to test")
+p.add_argument("-m", "--module", choices=["stype", "coref", "ace", "rst"], default="ace", help="module to test")
 
 opts = p.parse_args()
 if opts.module == "coref":
@@ -14,3 +15,5 @@ elif opts.module == "stype":
 	test_stype()
 elif opts.module == "ace":
 	test_ace()
+elif opts.module == "rst":
+	test_rst()
