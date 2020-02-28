775c923a63ba7ddae62ab4b266a18e5e58e35bed
Yilun Zhu <allen.yl.zhu@gmail.com>
2020-02-28 16:42:27 -0500

    dep parser supporting files

---
+++ lib/dep_parsing/depparse_processor.py
@@ -0,0 +1,43 @@
+"""
+Processor for performing dependency parsing
+"""
+
+from stanfordnlp.models.common.pretrain import Pretrain
+from stanfordnlp.models.common.utils import unsort
+from stanfordnlp.models.depparse.data import DataLoader
+from stanfordnlp.models.depparse.trainer import Trainer
+from stanfordnlp.pipeline._constants import *
+from stanfordnlp.pipeline.processor import UDProcessor
+
+
+class DepparseProcessor(UDProcessor):
+
+    # set of processor requirements this processor fulfills
+    PROVIDES_DEFAULT = set([DEPPARSE])
+    # set of processor requirements for this processor
+    REQUIRES_DEFAULT = set([TOKENIZE, POS])
+
+    def __init__(self, config, pipeline, use_gpu):
+        self._pretagged = None
+        super().__init__(config, pipeline, use_gpu)
+
+    def _set_up_requires(self):
+        if self._pretagged:
+            self._requires = set()
+        else:
+            self._requires = self.__class__.REQUIRES_DEFAULT
+    
+    def _set_up_model(self, config, use_gpu):
+        self._pretagged = config.get('pretagged')
+        self._pretrain = Pretrain(config['pretrain_path'])
+        self._trainer = Trainer(pretrain=self.pretrain, model_file=config['model_path'], use_cuda=use_gpu)
+
+    def process(self, doc):
+        batch = DataLoader(
+            doc, self.config['batch_size'], self.config, self.pretrain, vocab=self.vocab, evaluation=True,
+            sort_during_eval=True)
+        preds = []
+        for i, b in enumerate(batch):
+            preds += self.trainer.predict(b)
+        preds = unsort(preds, batch.data_orig_idx)
+        batch.conll.set(['head', 'deprel'], [y for x in preds for y in x])
